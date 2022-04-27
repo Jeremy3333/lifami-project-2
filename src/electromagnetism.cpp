c3a4add27a00dbbb3af359c0d87c43ae1525dabf
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-04-27 22:48:03 +0200

    Added interpolation

--- src/electromagnetism.cpp
+++ src/electromagnetism.cpp
@@ -18,7 +18,7 @@ void initElectromagnetism(Electromagnetism &e, Vector2f position, Vector2f veloc
 void initElectromagnetismArray(ElectromagnetismArray &e)
 {
     e.nbParticles = 1;
-    initElectromagnetism(e.particles[0], initVector2f(0, 0), initVector2f(0, 0), 20, {0, 255, 0, 255});
+    initElectromagnetism(e.particles[0], initVector2f(0, 0), initVector2f(0, 0), 100, {0, 255, 0, 255});
     e.loaded = true;
 }
 
@@ -37,47 +37,57 @@ int countInferior(float point[4], float answer, int index[4])
     return nb;
 }
 
-void drawElectromagnetismArray(ElectromagnetismArray e, RenderWindow &window)
+// return (c * d) / a
+float ruleOfThree(float a, float b, float c)
 {
-    int x, i, j;
-    int numInf, index[4];
+    return (c * b) / a;
+}
+
+// calculate the actualle equation at the point
+void calculateEquation(float beta[5], float & r_sqr, Electromagnetism particle, int i, int j)
+{
+    int nb = 0;
+    r_sqr = pow(particle.radius, 2);
+    for(int i2 = i; i2 <= i + 1; i2++)
+    {
+        for(int j2 = j; j2 >= j - 1; j2--)
+        {
+            beta[nb] = pow(i2 - particle.position.x, 2) + pow(j2 - particle.position.y, 2);
+            nb++;
+        }
+    }
+    beta[4] = pow(i - particle.position.x + 0.5, 2) + pow(j - particle.position.y + 0.5, 2);
+}
+
+void drawCalculedLine(RenderWindow &window, Electromagnetism particle, int i, int j)
+{
+    int numInf;
+    int index[4];
     float beta[5];
     float r_sqr;
     Vector2f drawPos[4];
-    for(x = 0; x < e.nbParticles; x++)
-    {
-        window.color(e.particles[x].color.r, e.particles[x].color.g, e.particles[x].color.b, e.particles[x].color.a);
-        r_sqr = pow(e.particles[x].radius, 2);
-        for(i = -((WINDOW_WIDTH / CASE_SIZE)/2); i < (WINDOW_WIDTH / CASE_SIZE)/2; i++)
-        {
-            for(j = -((WINDOW_HEIGHT / CASE_SIZE)/2); j < (WINDOW_HEIGHT / CASE_SIZE)/2; j++)
-            {
-                beta[0] = pow(i - e.particles[x].position.x, 2) + pow(j - e.particles[x].position.y, 2);
-                beta[1] = pow(i - e.particles[x].position.x, 2) + pow(j - e.particles[x].position.y - 1, 2);
-                beta[2] = pow(i - e.particles[x].position.x + 1, 2) + pow(j - e.particles[x].position.y, 2);
-                beta[3] = pow(i - e.particles[x].position.x + 1, 2) + pow(j - e.particles[x].position.y - 1, 2);
-                beta[4] = pow(i - e.particles[x].position.x + 0.5, 2) + pow(j - e.particles[x].position.y + 0.5, 2);
-                numInf = countInferior(beta, r_sqr, index);
-                drawPos[0] = getDrawPosition(initVector2f(i * CASE_SIZE, j * CASE_SIZE), initVector2f(0, 0));
-                drawPos[1] = getDrawPosition(initVector2f(i * CASE_SIZE, (j - 1) * CASE_SIZE), initVector2f(0, 0));
-                drawPos[2] = getDrawPosition(initVector2f((i + 1) * CASE_SIZE, j * CASE_SIZE), initVector2f(0, 0));
-                drawPos[3] = getDrawPosition(initVector2f((i + 1) * CASE_SIZE, (j - 1) * CASE_SIZE), initVector2f(0, 0));
-                switch (numInf)
+    calculateEquation(beta, r_sqr, particle, i, j);
+    drawPos[0] = getDrawPosition(initVector2f(i * CASE_SIZE, j * CASE_SIZE), initVector2f(0, 0));
+    drawPos[1] = getDrawPosition(initVector2f(i * CASE_SIZE, (j - 1) * CASE_SIZE), initVector2f(0, 0));
+    drawPos[2] = getDrawPosition(initVector2f((i + 1) * CASE_SIZE, j * CASE_SIZE), initVector2f(0, 0));
+    drawPos[3] = getDrawPosition(initVector2f((i + 1) * CASE_SIZE, (j - 1) * CASE_SIZE), initVector2f(0, 0));
+    numInf = countInferior(beta, r_sqr, index);
+    switch (numInf)
                 {
                 case 1:
                     switch (index[0])
                     {
                     case 0:
-                        window.drawLine(drawPos[0].x + CASE_SIZE / 2, drawPos[0].y, drawPos[0].x, drawPos[0].y - CASE_SIZE / 2);
+                        window.drawLine(drawPos[0].x + ruleOfThree(beta[2] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[0].y, drawPos[0].x, drawPos[0].y - ruleOfThree(beta[1] - beta[0], CASE_SIZE, r_sqr - beta[0]));
                         break;
                     case 1:
-                        window.drawLine(drawPos[1].x + CASE_SIZE / 2, drawPos[1].y, drawPos[1].x, drawPos[1].y + CASE_SIZE / 2);
+                        window.drawLine(drawPos[1].x + ruleOfThree(beta[3] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[1].y, drawPos[1].x, drawPos[1].y + ruleOfThree(beta[0] - beta[1], CASE_SIZE, r_sqr - beta[1]));
                         break;
                     case 2:
-                        window.drawLine(drawPos[2].x - CASE_SIZE / 2, drawPos[2].y, drawPos[2].x, drawPos[2].y - CASE_SIZE / 2);
+                        window.drawLine(drawPos[2].x - ruleOfThree(beta[0] - beta[2], CASE_SIZE, r_sqr - beta[2]), drawPos[2].y, drawPos[2].x, drawPos[2].y - ruleOfThree(beta[3] - beta[2], CASE_SIZE, r_sqr - beta[2]));
                         break;
                     case 3:
-                        window.drawLine(drawPos[3].x - CASE_SIZE / 2, drawPos[3].y, drawPos[3].x, drawPos[3].y + CASE_SIZE / 2);
+                        window.drawLine(drawPos[3].x - ruleOfThree(beta[1] - beta[3], CASE_SIZE, r_sqr - beta[3]), drawPos[3].y, drawPos[3].x, drawPos[3].y + ruleOfThree(beta[2] - beta[3], CASE_SIZE, r_sqr - beta[3]));
                         break;
                     default:
                         break;
@@ -86,67 +96,80 @@ void drawElectromagnetismArray(ElectromagnetismArray e, RenderWindow &window)
                 case 2:
                     if (index[0] == 0 && index[1] == 1)
                     {
-                        window.drawLine(drawPos[0].x + CASE_SIZE / 2, drawPos[0].y, drawPos[1].x + CASE_SIZE / 2, drawPos[1].y);
+                        window.drawLine(drawPos[0].x + ruleOfThree(beta[2] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[0].y, drawPos[1].x + ruleOfThree(beta[3] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[1].y);
                     }
                     else if(index[0] == 0 && index[1] == 2)
                     {
-                        window.drawLine(drawPos[0].x, drawPos[0].y - CASE_SIZE / 2, drawPos[2].x, drawPos[2].y - CASE_SIZE / 2);
+                        window.drawLine(drawPos[0].x, drawPos[0].y - ruleOfThree(beta[1] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[2].x, drawPos[2].y - ruleOfThree(beta[3] - beta[2], CASE_SIZE, r_sqr - beta[2]));
                     }
                     else if(index[0] == 0 && index[1] == 3)
                     {
-                        if(beta[4] <= r_sqr)
+                        if(beta[4] > r_sqr)
                         {
-                            window.drawLine(drawPos[1].x + CASE_SIZE / 2, drawPos[1].y, drawPos[1].x, drawPos[1].y + CASE_SIZE / 2);
-                            window.drawLine(drawPos[2].x - CASE_SIZE / 2, drawPos[2].y, drawPos[2].x, drawPos[2].y - CASE_SIZE / 2);
+                            window.drawLine(drawPos[1].x + ruleOfThree(beta[3] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[1].y, drawPos[1].x, drawPos[1].y + ruleOfThree(beta[0] - beta[1], CASE_SIZE, r_sqr - beta[1]));
+                            window.drawLine(drawPos[2].x - ruleOfThree(beta[0] - beta[2], CASE_SIZE, r_sqr - beta[2]), drawPos[2].y, drawPos[2].x, drawPos[2].y - ruleOfThree(beta[3] - beta[2], CASE_SIZE, r_sqr - beta[2]));
                         }
                         else
                         {
-                            window.drawLine(drawPos[0].x + CASE_SIZE / 2, drawPos[0].y, drawPos[0].x, drawPos[0].y - CASE_SIZE / 2);
-                            window.drawLine(drawPos[3].x - CASE_SIZE / 2, drawPos[3].y, drawPos[3].x, drawPos[3].y + CASE_SIZE / 2);
+                            window.drawLine(drawPos[0].x + ruleOfThree(beta[2] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[0].y, drawPos[0].x, drawPos[0].y - ruleOfThree(beta[1] - beta[0], CASE_SIZE, r_sqr - beta[0]));
+                            window.drawLine(drawPos[3].x - ruleOfThree(beta[1] - beta[3], CASE_SIZE, r_sqr - beta[3]), drawPos[3].y, drawPos[3].x, drawPos[3].y + ruleOfThree(beta[2] - beta[3], CASE_SIZE, r_sqr - beta[3]));
                         }
                     }
                     else if(index[0] == 1 && index[1] == 2)
                     {
-                        if(beta[4] <= r_sqr)
+                        if(beta[4] > r_sqr)
                         {
-                            window.drawLine(drawPos[0].x + CASE_SIZE / 2, drawPos[0].y, drawPos[0].x, drawPos[0].y - CASE_SIZE / 2);
-                            window.drawLine(drawPos[3].x - CASE_SIZE / 2, drawPos[3].y, drawPos[3].x, drawPos[3].y + CASE_SIZE / 2);
+                            window.drawLine(drawPos[0].x + ruleOfThree(beta[2] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[0].y, drawPos[0].x, drawPos[0].y - ruleOfThree(beta[1] - beta[0], CASE_SIZE, r_sqr - beta[0]));
+                            window.drawLine(drawPos[3].x - ruleOfThree(beta[1] - beta[3], CASE_SIZE, r_sqr - beta[3]), drawPos[3].y, drawPos[3].x, drawPos[3].y + ruleOfThree(beta[2] - beta[3], CASE_SIZE, r_sqr - beta[3]));
                         }
                         else
                         {
-                            window.drawLine(drawPos[1].x + CASE_SIZE / 2, drawPos[1].y, drawPos[1].x, drawPos[1].y + CASE_SIZE / 2);
-                            window.drawLine(drawPos[2].x - CASE_SIZE / 2, drawPos[2].y, drawPos[2].x, drawPos[2].y - CASE_SIZE / 2);
+                            window.drawLine(drawPos[1].x + ruleOfThree(beta[3] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[1].y, drawPos[1].x, drawPos[1].y + ruleOfThree(beta[0] - beta[1], CASE_SIZE, r_sqr - beta[1]));
+                            window.drawLine(drawPos[2].x - ruleOfThree(beta[0] - beta[2], CASE_SIZE, r_sqr - beta[2]), drawPos[2].y, drawPos[2].x, drawPos[2].y - ruleOfThree(beta[3] - beta[2], CASE_SIZE, r_sqr - beta[2]));
                         }
                     }
                     else if(index[0] == 1 && index[1] == 3)
                     {
-                        window.drawLine(drawPos[1].x, drawPos[1].y + CASE_SIZE / 2, drawPos[3].x, drawPos[3].y + CASE_SIZE / 2);
+                        window.drawLine(drawPos[1].x, drawPos[1].y + ruleOfThree(beta[0] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[3].x, drawPos[3].y + ruleOfThree(beta[2] - beta[3], CASE_SIZE, r_sqr - beta[3]));
                     }
                     else if (index[0] == 2 && index[1] == 3)
                     {
-                        window.drawLine(drawPos[2].x - CASE_SIZE / 2, drawPos[2].y, drawPos[3].x - CASE_SIZE / 2, drawPos[3].y);
+                        window.drawLine(drawPos[2].x - ruleOfThree(beta[0] - beta[2], CASE_SIZE, r_sqr - beta[2]), drawPos[2].y, drawPos[3].x -ruleOfThree(beta[1] - beta[3], CASE_SIZE, r_sqr - beta[3]), drawPos[3].y);
                     }
                     break;
                 case 3:
                     if(index[0] == 0 && index[1] == 1 && index[2] == 2)
                     {
-                        window.drawLine(drawPos[3].x - CASE_SIZE / 2, drawPos[3].y, drawPos[3].x, drawPos[3].y + CASE_SIZE / 2);
+                        window.drawLine(drawPos[3].x - ruleOfThree(beta[1] - beta[3], CASE_SIZE, r_sqr - beta[3]), drawPos[3].y, drawPos[3].x, drawPos[3].y + ruleOfThree(beta[2] - beta[3], CASE_SIZE, r_sqr - beta[3]));
                     }
                     else if(index[0] == 0 && index[1] == 1 && index[2] == 3)
                     {
-                        window.drawLine(drawPos[2].x - CASE_SIZE / 2, drawPos[2].y, drawPos[2].x, drawPos[2].y - CASE_SIZE / 2);
+                        window.drawLine(drawPos[2].x - ruleOfThree(beta[0] - beta[2], CASE_SIZE, r_sqr - beta[2]), drawPos[2].y, drawPos[2].x, drawPos[2].y - ruleOfThree(beta[3] - beta[2], CASE_SIZE, r_sqr - beta[2]));
                     }
                     else if(index[0] == 0 && index[1] == 2 && index[2] == 3)
                     {
-                        window.drawLine(drawPos[1].x + CASE_SIZE / 2, drawPos[1].y, drawPos[1].x, drawPos[1].y + CASE_SIZE / 2);
+                        window.drawLine(drawPos[1].x + ruleOfThree(beta[3] - beta[1], CASE_SIZE, r_sqr - beta[1]), drawPos[1].y, drawPos[1].x, drawPos[1].y + ruleOfThree(beta[0] - beta[1], CASE_SIZE, r_sqr - beta[1]));
                     }
                     else if(index[0] == 1 && index[1] == 2 && index[2] == 3)
                     {
-                        window.drawLine(drawPos[0].x + CASE_SIZE / 2, drawPos[0].y, drawPos[0].x, drawPos[0].y - CASE_SIZE / 2);
+                        window.drawLine(drawPos[0].x + ruleOfThree(beta[2] - beta[0], CASE_SIZE, r_sqr - beta[0]), drawPos[0].y, drawPos[0].x, drawPos[0].y - ruleOfThree(beta[1] - beta[0], CASE_SIZE, r_sqr - beta[0]));
                     }
                 default:
                     break;
                 }
+}
+
+void drawElectromagnetismArray(ElectromagnetismArray e, RenderWindow &window)
+{
+    int x, i, j;
+    for(x = 0; x < e.nbParticles; x++)
+    {
+        for(i = -((WINDOW_WIDTH / CASE_SIZE)/2); i < (WINDOW_WIDTH / CASE_SIZE)/2; i++)
+        {
+            for(j = -((WINDOW_HEIGHT / CASE_SIZE)/2); j < (WINDOW_HEIGHT / CASE_SIZE)/2; j++)
+            {
+            window.color(e.particles[x].color.r, e.particles[x].color.g, e.particles[x].color.b, e.particles[x].color.a);
+                drawCalculedLine(window, e.particles[x], i, j);
             }
         }
     }
